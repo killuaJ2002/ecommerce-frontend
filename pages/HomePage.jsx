834931ddb714fff3de6f8a56bf7cc455fea246b6
killuaJ2002 <kjcodes2002@gmail.com>
2025-08-09 01:49:13 +0530

    built navbar and styled it

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import styles from "./HomePage.module.css";
 const HomePage = () => {
   const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
@@ -11,19 +12,23 @@ const HomePage = () => {
 
   return (
     <>
-      // Navbar
-      <div className="navbar">
-        <div className="navbar-left">left of navbar</div>
-        <div className="navbar-right">right of navbar</div>
+      {/* Navbar */}
+      <div className={styles.navbar}>
+        <div className={styles.navbar_left}>FlopKart</div>
+        <div className={styles.navbar_right}>
+          <Link className={styles.navLink}>Orders</Link>
+          <Link className={styles.navLink}>Cart</Link>
+          {isAuthenticated() ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <Link className={styles.navLink} to="/login">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
 };
 
 export default HomePage;
-
-// {isAuthenticated() ? (
-//         <button onClick={handleLogout}>Logout</button>
-//       ) : (
-//         <Link to="/login">Login</Link>
-//       )}
