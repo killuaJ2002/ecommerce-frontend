d0ab1fee2a8ec75ace314df1385011a7be6707e3
killuaJ2002 <kjcodes2002@gmail.com>
2025-08-13 23:13:23 +0530

    moved context and pages folders inside src

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import {
   RouterProvider,
   Navigate,
 } from "react-router-dom";
-import LoginPage from "../pages/LogInPage";
-import SignUpPage from "../pages/SignUpPage";
-import HomePage from "../pages/HomePage";
-import { AuthProvider } from "../context/AuthContext";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import { AuthProvider } from "./context/AuthContext";
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<LogInPage />} />
         <Route path="signup" element={<SignUpPage />} />
         <Route index element={<HomePage />} />
       </>
